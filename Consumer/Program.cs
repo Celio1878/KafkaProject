582d3c51f2f2269b342a0c63a8e2f804adab0641
Celio Vieira <celio1878@hotmail.com>
2023-11-06 03:44:21 -0300

    feat: Create project

---
+++ Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using deviocourse;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ConsumerConfig
+{
+    GroupId = "devio",
+    BootstrapServers = "localhost:9092"
+};
+
+var deserializer = new AvroDeserializer<learning>(schemaRegistry).AsSyncOverAsync();
+using var consumer = new ConsumerBuilder<string, learning>(config).SetValueDeserializer(deserializer).Build();
+
+const string topicName = "deviocourse";
+consumer.Subscribe(topicName);
+
+var result = consumer.Consume();
+
+Console.WriteLine($"Message {result.Message.Key} with value -> {result.Message.Value.id}");
---
+++ Producer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using deviocourse;
+
+var schemaConfig = new SchemaRegistryConfig
+{
+    Url = "http://localhost:8081"
+};
+
+var schemaRegistry = new CachedSchemaRegistryClient(schemaConfig);
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+var avroSerializer = new AvroSerializer<learning>(schemaRegistry);
+var producerBuilder = new ProducerBuilder<string, learning>(config);
+producerBuilder.SetValueSerializer(avroSerializer);
+using var producer = producerBuilder.Build();
+
+Message<string, learning> message = new Message<string, learning>
+{
+    Key = Guid.NewGuid().ToString(),
+    Value = new learning
+    {
+        id = Guid.NewGuid().ToString(),
+        describe = "New Schema",
+        date = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+    }
+};
+
+const string topicName = "deviocourse";
+var result = await producer.ProduceAsync(topicName, message);
+
+Console.WriteLine($"Message sent to topic {topicName} with offset {result.Offset}");
